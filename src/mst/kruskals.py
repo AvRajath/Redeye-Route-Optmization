021f58b97b1706f1625cfafe20e3c8f1203f2ed8
kolse512 <130181505+kolse512@users.noreply.github.com>
2023-04-15 23:36:14 -0400

    Update kruskals.py

--- src/mst/kruskals.py
+++ src/mst/kruskals.py
@@ -46,17 +46,3 @@ class MST:
 
     def getEdges(self):
         return self.edges
-
-
-# example graph represented by an adjacency matrix
-# graph = [[0, 2, 7, 6, 3],
-#          [2, 0, 3, 8, 5],
-#          [1, 3, 0, 2, 7],
-#          [6, 8, 1, 0, 9],
-#          [1, 5, 7, 9, 0]]
-#
-# # find the minimum spanning tree using Kruskal's algorithm
-# # kruskalMST(graph)
-#
-# mst = MST(5)
-# mst.kruskalMST(graph)
