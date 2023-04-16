df251c86d85c3f7168abad89fa43c7cc2d89ae20
kolse512 <130181505+kolse512@users.noreply.github.com>
2023-04-15 23:31:40 -0400

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -6,6 +6,15 @@ class RouteGraph:
         self.gmaps = googlemaps.Client(key=constants.Constants.GOOGLEAPIKEY.value)
 
     def form_adjacency_matrix_time(self, row):
+        """
+        Forms adjacency matrix for the graph with time values
+        
+        Parameters:
+        row : data row with map locations
+
+        Returns:
+        adjacency matrix with time values
+        """
         adj_mat_time = []
         for i in range(len(row)):
             temp_sub_mat_time = []
@@ -19,6 +28,15 @@ class RouteGraph:
         return adj_mat_time
 
     def form_adjacency_matrix_distance(self, row):
+        """
+        Forms adjacency matrix for the graph with distance values
+        
+        Parameters:
+        row : data row with map locations
+
+        Returns:
+        adjacency matrix with distance values
+        """
         adj_mat_dist = []
         for i in range(len(row)):
             temp_sub_mat_dist = []
